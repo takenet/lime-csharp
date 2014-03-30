5057ac8c8f654f0c28f911de74bd4bced578e38d
André Bires <andrebires@gmail.com>
2014-03-29 23:55:30 -0300

    Tcp transport project

---
+++ src/Lime.Protocol.Tcp/TcpTransport.cs
@@ -0,0 +1,93 @@
+﻿using Lime.Protocol.Network;
+using Lime.Protocol.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lime.Protocol.Tcp
+{
+    /// <summary>
+    /// Provides the messaging protocol 
+    /// transport for TCP connections
+    /// </summary>
+    public class TcpTransport : ITransport
+    {
+        private readonly IEnvelopeSerializer _serializer;
+        private readonly ITraceWriter _traceWriter;
+
+        private TcpClient _tcpClient;
+        private NetworkStream _networkStream;
+        private Stream _stream;
+
+        private X509Certificate _sslCertificate;
+        private string _hostName;
+
+        private int _pos;
+        private byte[] _buffer;
+        const int DEFAULT_BUFFER_SIZE = 8192;
+
+
+        public TcpTransport(IEnvelopeSerializer serializer, ITraceWriter traceWriter)
+        {
+
+        }
+
+        #region ITransport Members
+
+        public Task SendAsync(Envelope envelope, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler<EnvelopeEventArgs<Envelope>> EnvelopeReceived;
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SessionCompression[] GetSupportedCompression()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SessionCompression Compression
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Task SetCompressionAsync(SessionCompression compression, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SessionEncryption[] GetSupportedEncryption()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SessionEncryption Encryption
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Task SetEncryptionAsync(SessionEncryption encryption, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler<ExceptionEventArgs> Failed;
+
+        public event EventHandler<DeferralEventArgs> Closing;
+
+        public event EventHandler Closed;
+
+        #endregion
+    }
+}
---
+++ src/Lime.Protocol/Network/ITraceWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lime.Protocol.Network
+{
+    /// <summary>
+    /// Enable request tracing
+    /// for network transports
+    /// </summary>
+    public interface ITraceWriter
+    {
+        /// <summary>
+        /// Trace some data
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="operation"></param>
+        /// <returns></returns>
+        Task TraceAsync(string data, DataOperation operation);
+
+        /// <summary>
+        /// Indicates if the tracer
+        /// is enabled
+        /// </summary>
+        bool IsEnabled { get; }
+    }
+
+    /// <summary>
+    /// Represents the transport
+    /// data operations
+    /// </summary>
+    public enum DataOperation
+    {
+        /// <summary>
+        /// A tranport send operation
+        /// </summary>
+        Send,
+        /// <summary>
+        /// A tranport receive operation
+        /// </summary>
+        Receive
+    }
+}
