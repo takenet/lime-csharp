0c0efa4718c1caa3716025be4f33652d8a988df5
André Bires <andrebires@gmail.com>
2014-06-10 17:31:26 -0300

    Missing file

---
+++ src/Lime.Client.TestConsole/ViewModels/EnvelopeViewModel.cs
@@ -0,0 +1,70 @@
+﻿using GalaSoft.MvvmLight;
+using Lime.Protocol.Network;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lime.Client.TestConsole.ViewModels
+{
+    public class EnvelopeViewModel : ViewModelBase
+    {
+
+        private string _json;
+
+        public string Json
+        {
+            get { return _json; }
+            set 
+            {
+                try
+                {
+                    var json = JObject.Parse(value);
+                    _json = json.ToString(Formatting.Indented);
+                }
+                catch
+                {
+
+                    _json = value;    
+                }
+                
+                RaisePropertyChanged(() => Json);
+            }
+        }
+
+        private DataOperation _direction;
+
+        public DataOperation Direction
+        {
+            get { return _direction; }
+            set 
+            { 
+                _direction = value;
+                RaisePropertyChanged(() => Direction);
+            }
+        }
+
+
+        private bool _isRaw;
+
+        /// <summary>
+        /// Indicates if the data 
+        /// was collected directly from
+        /// the transport trace writer
+        /// </summary>
+        public bool IsRaw
+        {
+            get { return _isRaw; }
+            set 
+            { 
+                _isRaw = value;
+                RaisePropertyChanged(() => IsRaw);
+            }
+        }
+
+
+    }
+}
