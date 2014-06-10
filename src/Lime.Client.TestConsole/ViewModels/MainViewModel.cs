533f11b073c41eb44465322ea21f4e2b92dfd147
André Bires <andrebires@gmail.com>
2014-06-10 17:31:13 -0300

    Initial commit of the test console

---
+++ src/Lime.Client.TestConsole/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using GalaSoft.MvvmLight;
+using Lime.Client.TestConsole.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lime.Client.TestConsole.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            this.SelectedSession = new SessionViewModel();
+        }
+
+
+        private SessionViewModel _selectedSession;
+
+        public SessionViewModel SelectedSession
+        {
+            get { return _selectedSession; }
+            set 
+            { 
+                _selectedSession = value;
+                RaisePropertyChanged(() => SelectedSession);
+            }
+        }
+
+        private ObservableCollectionEx<SessionViewModel> _sessions;
+
+        public ObservableCollectionEx<SessionViewModel> Sessions
+        {
+            get { return _sessions; }
+            set 
+            { 
+                _sessions = value;
+                RaisePropertyChanged(() => Sessions);
+            }
+        }
+
+    }
+}
---
+++ src/Lime.Client.TestConsole/ViewModels/SessionViewModel.cs
@@ -0,0 +1,57 @@
+﻿using GalaSoft.MvvmLight;
+using Lime.Client.TestConsole.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lime.Client.TestConsole.ViewModels
+{
+    public class SessionViewModel : ViewModelBase
+    {
+
+        private string _inputJson;
+
+        public string InputJson
+        {
+            get { return _inputJson; }
+            set
+            {
+                _inputJson = value;
+                RaisePropertyChanged(() => InputJson);
+            }
+        }
+
+        private string _hostUri;
+
+        public string HostUri
+        {
+            get { return _hostUri; }
+            set 
+            { 
+                _hostUri = value;
+                RaisePropertyChanged(() => HostUri);
+            }
+        }
+
+        private ObservableCollectionEx<EnvelopeViewModel> _envelopes;
+
+        public ObservableCollectionEx<EnvelopeViewModel> Envelopes
+        {
+            get { return _envelopes; }
+            set
+            {
+                _envelopes = value;
+                RaisePropertyChanged(() => Envelopes);
+            }
+        }
+
+
+
+        public AsyncCommand OpenTransportCommand { get; private set; }
+
+        public AsyncCommand CloseTransportCommand { get; private set; }
+
+    }
+}
