2c81b7d91aad840ce84cfe0df0ade0a5b18720e0
GabrielBico <gabriel.bico@gmail.com>
2014-06-18 09:00:29 -0300

    migrating to shared projects

--- src/Lime.Protocol.Tcp/Lime.Protocol.Tcp.Net45/Properties/AssemblyInfo.cs
+++ src/Lime.Protocol.Tcp/Lime.Protocol.Tcp.Net45/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lime.Protocol.WinPhone")]
+[assembly: AssemblyTitle("Lime.Protocol.Tcp.Net45")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lime.Protocol.WinPhone")]
+[assembly: AssemblyProduct("Lime.Protocol.Tcp.Net45")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,7 +20,7 @@ using System.Resources;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("65077432-0c92-466b-b68d-911a8ec84f1d")]
+[assembly: Guid("e53d775e-f7e4-4bb7-aff7-5413a5eaab27")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,8 +29,8 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
