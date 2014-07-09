902cd28e7f8145e381e11718d1ac8b255e3bdf1d
André Bires <andrebires@gmail.com>
2014-07-09 15:11:28 -0300

    Cancellation of pending receive tasks on transport failure

--- src/Lime.Protocol/Lime.Protocol/Network/ChannelBase.cs
+++ src/Lime.Protocol/Lime.Protocol/Network/ChannelBase.cs
@@ -318,13 +318,13 @@ namespace Lime.Protocol.Network
             {
                 throw new InvalidOperationException(string.Format("Cannot receive a session in the '{0}' session state", this.State));
             }
-
-            var combinedCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(
-                _channelCancellationTokenSource.Token,
-                cancellationToken);            
-
+       
             if (this.State == SessionState.Established)
             {
+                var combinedCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(
+                    _channelCancellationTokenSource.Token,
+                    cancellationToken);     
+
                 if (_consumeTransportTask.IsFaulted)
                 {
                     throw _consumeTransportTask.Exception.InnerException;
@@ -352,34 +352,44 @@ namespace Lime.Protocol.Network
 
         private async Task ConsumeTransportAsync()
         {
-            while (!_channelCancellationTokenSource.IsCancellationRequested && 
-                    this.State == SessionState.Established)
+            try
             {
-                Exception exception = null;
+                while (!_channelCancellationTokenSource.IsCancellationRequested &&
+                        this.State == SessionState.Established)
+                {
+                    Exception exception = null;
 
-                try
-                {
-                    var envelope = await this.ReceiveAsync(_channelCancellationTokenSource.Token).ConfigureAwait(false);                    
-                    await this.PostEnvelopeToBufferAsync(envelope);
-                }
-                catch (OperationCanceledException) { }
-                catch (ObjectDisposedException)
-                {
-                    if (!_isDisposing)
+                    try
                     {
-                        throw;
+                        var envelope = await this.ReceiveAsync(_channelCancellationTokenSource.Token).ConfigureAwait(false);
+                        await this.PostEnvelopeToBufferAsync(envelope);
+                    }
+                    catch (OperationCanceledException) { }
+                    catch (ObjectDisposedException)
+                    {
+                        if (!_isDisposing)
+                        {
+                            throw;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        exception = ex;
+                    }
+
+                    if (exception != null)
+                    {
+                        var cts = new CancellationTokenSource(_sendTimeout);
+                        await this.Transport.CloseAsync(cts.Token).ConfigureAwait(false);
+                        throw exception;
                     }
                 }
-                catch (Exception ex)
+            }
+            finally
+            {
+                if (!_channelCancellationTokenSource.IsCancellationRequested)
                 {
-                    exception = ex;
-                }
-
-                if (exception != null)
-                {
-                    var cts = new CancellationTokenSource(_sendTimeout);
-                    await this.Transport.CloseAsync(cts.Token).ConfigureAwait(false);
-                    throw exception;
+                    _channelCancellationTokenSource.Cancel();
                 }
             }
         }
