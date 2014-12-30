298e6179780730e7034c39eb276ca08908d0182c
André Bires <andrebires@gmail.com>
2014-12-30 10:45:40 -0200

    TraceWriter implementation. TaskUtil completed task.

---
+++ src/Lime.Protocol/Lime.Protocol/Network/TraceWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Lime.Protocol.Util;
+
+namespace Lime.Protocol.Network
+{
+    /// <summary>
+    /// Traces into the default .NET tracers.
+    /// </summary>
+    public sealed class TraceWriter : ITraceWriter
+    {
+        #region ITraceWriter Members
+
+        public Task TraceAsync(string data, DataOperation operation)
+        {
+            Trace.WriteLine(data, operation.ToString());
+            return TaskUtil.CompletedTask;
+        }
+
+        public bool IsEnabled
+        {
+            get { return Trace.Listeners.Count > 0; }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Lime.Protocol/Lime.Protocol/Util/TaskUtil.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Lime.Protocol.Util
+{
+    public static class TaskUtil
+    {
+        /// <summary>
+        /// Get a completed task.
+        /// </summary>
+        public readonly static Task CompletedTask = Task.FromResult<object>(null);
+    }
+}
