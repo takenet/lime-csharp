e67b829c0bbee768a133745074db0bfa00ac9a95
André Bires <andrebires@gmail.com>
2015-09-22 21:35:26 -0300

    Added TypeScript targets.

--- src/Lime.Transport.WebSocket/lime.js
+++ src/Lime.Transport.WebSocket/lime.js
@@ -1,14 +1,11 @@
-﻿function establishSession() {
+function establishSession() {
     var webSocket = new WebSocket("ws://localhost", "lime");
-
     webSocket.onmessage = function (event) {
         console.log(event.data);
     };
-
-    webSocket.onopen = function(event) {
-        webSocket.send(
-            JSON.stringify({
-                state: "new"
-            }));
-    }
+    webSocket.onopen = function (event) {
+        webSocket.send(JSON.stringify({
+            state: "new"
+        }));
+    };
 }
