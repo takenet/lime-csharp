9ab63ff2963a41a5136f9410c985b37566cd8010
André Bires Fonseca <andrebires@gmail.com>
2016-01-14 10:24:33 -0200

    Removed shared projects and platform specific projects

---
+++ src/Lime.Transport.Tcp/Lime.Transport.Tcp.Net45/JsonBuffer.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Text;
+
+namespace Lime.Transport
+{
+    /// <summary>
+    /// Provides a buffer with a method for JSON extraction.
+    /// </summary>
+    internal sealed class JsonBuffer
+    {
+        private readonly byte[] _buffer;
+        private int _bufferCurPos;
+        private int _jsonStartPos;
+        private int _jsonCurPos;
+        private int _jsonStackedBrackets;
+        private bool _jsonStarted;
+        
+        public JsonBuffer(int bufferSize)
+        {
+            _buffer = new byte[bufferSize];
+        }
+
+        public byte[] Buffer => _buffer;
+
+        public int BufferCurPos
+        {
+            set { _bufferCurPos = value; }
+            get { return _bufferCurPos; }
+        }
+
+        /// <summary>
+        /// Try to extract a JSON document from the buffer, based on the brackets count.
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public bool TryExtractJsonFromBuffer(out byte[] json)
+        {
+            if (_bufferCurPos > _buffer.Length)
+            {
+                throw new ArgumentException("Buffer current pos or length value is invalid");
+            }
+
+            json = null;
+            int jsonLenght = 0;
+
+            for (int i = _jsonCurPos; i < _bufferCurPos; i++)
+            {
+                _jsonCurPos = i + 1;
+
+                if (_buffer[i] == '{')
+                {
+                    _jsonStackedBrackets++;
+                    if (!_jsonStarted)
+                    {
+                        _jsonStartPos = i;
+                        _jsonStarted = true;
+                    }
+                }
+                else if (_buffer[i] == '}')
+                {
+                    _jsonStackedBrackets--;
+                }
+
+                if (_jsonStarted &&
+                    _jsonStackedBrackets == 0)
+                {
+                    jsonLenght = i - _jsonStartPos + 1;
+                    break;
+                }
+            }
+
+            if (jsonLenght > 1)
+            {
+                json = new byte[jsonLenght];
+                Array.Copy(_buffer, _jsonStartPos, json, 0, jsonLenght);
+
+                // Shifts the buffer to the left
+                _bufferCurPos -= (jsonLenght + _jsonStartPos);
+                Array.Copy(_buffer, jsonLenght + _jsonStartPos, _buffer, 0, _bufferCurPos);
+
+                _jsonCurPos = 0;
+                _jsonStartPos = 0;
+                _jsonStarted = false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return Encoding.UTF8.GetString(_buffer);
+        }
+    }
+}
