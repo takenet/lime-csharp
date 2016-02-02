052a229c2024c475956b6f94c9c31af72284ca44
André Bires <andrebires@gmail.com>
2016-02-02 11:58:30 -0200

    Added DataflowChannelAdapter implementation

---
+++ src/Lime.Protocol/Adapters/DataFlowChannelAdapter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Lime.Protocol.Network;
+
+namespace Lime.Protocol.Adapters
+{
+    public sealed class DataflowChannelAdapter : IDisposable
+    {        
+        private readonly CancellationTokenSource _cts;
+
+        public DataflowChannelAdapter(IChannel channel)
+            : this(channel, channel, channel)
+        {
+            
+        }
+
+        public DataflowChannelAdapter(IChannel channel, DataflowBlockOptions dataflowBlockOptions)
+            : this(channel, channel, channel, dataflowBlockOptions)
+        {
+
+        }
+
+        public DataflowChannelAdapter(IMessageChannel messageChannel, INotificationChannel notificationChannel, ICommandChannel commandChannel)
+            : this(messageChannel, notificationChannel, commandChannel, new DataflowBlockOptions())
+        {
+            
+        }
+
+        public DataflowChannelAdapter(IMessageChannel messageChannel, INotificationChannel notificationChannel, ICommandChannel commandChannel, DataflowBlockOptions dataflowBlockOptions)
+        {
+            if (messageChannel == null) throw new ArgumentNullException(nameof(messageChannel));
+            if (notificationChannel == null) throw new ArgumentNullException(nameof(notificationChannel));
+            if (commandChannel == null) throw new ArgumentNullException(nameof(commandChannel));
+            MessageBufferBlock = new BufferBlock<Message>(dataflowBlockOptions);
+            NotificationBufferBlock = new BufferBlock<Notification>(dataflowBlockOptions);
+            CommandBufferBlock = new BufferBlock<Command>(dataflowBlockOptions);
+            _cts = new CancellationTokenSource();
+
+            MessageListenerTask = Listener.ListenAsync(messageChannel.ReceiveMessageAsync,
+                MessageBufferBlock.SendAsync, _cts.Token);
+            NotificationListenerTask = Listener.ListenAsync(notificationChannel.ReceiveNotificationAsync,
+                NotificationBufferBlock.SendAsync, _cts.Token);
+            CommandListenerTask = Listener.ListenAsync(commandChannel.ReceiveCommandAsync,
+                CommandBufferBlock.SendAsync, _cts.Token);
+        }
+
+        public Task MessageListenerTask { get; }
+        public Task NotificationListenerTask { get; }
+        public Task CommandListenerTask { get; }
+
+        public BufferBlock<Message> MessageBufferBlock { get; }
+
+        public BufferBlock<Notification> NotificationBufferBlock { get; }
+
+        public BufferBlock<Command> CommandBufferBlock { get; }
+
+        public void Dispose()
+        {
+            _cts.Cancel();
+            _cts.Dispose();
+            MessageListenerTask.Dispose();
+            NotificationListenerTask.Dispose();
+            CommandListenerTask.Dispose();            
+        }
+    }
+}
---
+++ src/Lime.Protocol/Adapters/Listener.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lime.Protocol.Adapters
+{
+    public static class Listener
+    {
+        /// <summary>
+        /// Creates a long running task that listens the provided listener and call the consumer until the cancellation is requested.
+        /// The token cancellation occurs silently.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="listener">The listener func.</param>
+        /// <param name="consumer">The consumer func.</param>
+        /// <param name="cancellationToken">The cancellation token to stop the listener. It must be a valid token.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// </exception>
+        /// <exception cref="System.ArgumentException">A valid cancellation token must be provided</exception>
+        public static Task ListenAsync<T>(Func<CancellationToken, Task<T>> listener, Func<T, Task> consumer, CancellationToken cancellationToken)
+        {
+            if (listener == null) throw new ArgumentNullException(nameof(listener));
+            if (consumer == null) throw new ArgumentNullException(nameof(consumer));            
+            if (cancellationToken == CancellationToken.None) throw new ArgumentException("A valid cancellation token must be provided", nameof(cancellationToken));
+            return Task.Factory.StartNew(async () =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        var item = await listener(cancellationToken);
+                        await consumer(item);
+                    }
+                    catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                }
+            },            
+            TaskCreationOptions.LongRunning);
+        }
+    }
+}
