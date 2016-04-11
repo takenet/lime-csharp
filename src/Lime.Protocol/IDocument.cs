03a3fc4d20e2c3398982fc8325931a70c9ee7896
André Bires <andrebires@gmail.com>
2016-04-11 09:53:26 -0300

    Added comments to the Document class.

--- src/Lime.Protocol/IDocument.cs
+++ src/Lime.Protocol/IDocument.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Lime.Protocol
 {
+    /// <summary>
+    /// Defines a entity with a <see cref="MediaType"/>.
+    /// </summary>
     public interface IDocument
     {
+        /// <summary>
+        /// Gets the type of the media for the document.
+        /// </summary>
+        /// <returns></returns>
         MediaType GetMediaType();
     }
 }
