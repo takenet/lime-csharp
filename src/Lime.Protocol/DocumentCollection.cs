3896abc3970cc07f440352aec0cca639ef70572c
André Bires <andrebires@gmail.com>
2016-04-12 17:48:33 -0300

    Added DocumentContainer class.

--- src/Lime.Protocol/DocumentCollection.cs
+++ src/Lime.Protocol/DocumentCollection.cs
@@ -4,53 +4,59 @@ using System.Runtime.Serialization;
 namespace Lime.Protocol
 {
     /// <summary>
-    /// Represents a collection
-    /// of documents.
+    /// Represents a collection of documents.
     /// </summary>
     [DataContract(Namespace = "http://limeprotocol.org/2014")]
     public class DocumentCollection : Document, IEnumerable
     {
         public const string MIME_TYPE = "application/vnd.lime.collection+json";
-
         public const string TOTAL_KEY = "total";
         public const string ITEM_TYPE_KEY = "itemType";
         public const string ITEMS_KEY = "items";
-        
-        #region Constructor
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocumentCollection"/> class.
+        /// </summary>
         public DocumentCollection()
             : base(MediaType.Parse(MIME_TYPE))
         {
 
         }
 
-        #endregion
-
-        #region Public Properties
-
+        /// <summary>
+        /// Gets or sets the total of items in the collection.
+        /// The count refers to the original source collection, without any applied filter that may be applied in the items on this collection.
+        /// </summary>
+        /// <value>
+        /// The total.
+        /// </value>
         [DataMember(Name = TOTAL_KEY, EmitDefaultValue = false)]
         public int Total { get; set; }
 
+        /// <summary>
+        /// Gets or sets the media type of all items of the collection..
+        /// </summary>
+        /// <value>
+        /// The type of the item.
+        /// </value>
         [DataMember(Name = ITEM_TYPE_KEY)]
         public MediaType ItemType { get; set; }
 
+        /// <summary>
+        /// Gets or sets the collection items.
+        /// </summary>
+        /// <value>
+        /// The items.
+        /// </value>
         [DataMember(Name = ITEMS_KEY)]
         public Document[] Items { get; set; }
 
-        #endregion
-
-        #region IEnumerable Members
-
-        public IEnumerator GetEnumerator()
-        {
-            if (Items != null)
-            {
-                return Items.GetEnumerator();
-            }
-
-            return null;
-        }
-
-        #endregion
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator GetEnumerator() => Items?.GetEnumerator();
     }
 }
---
+++ src/Lime.Protocol/DocumentContainer.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.Serialization;
+
+namespace Lime.Protocol
+{
+    /// <summary>
+    /// Represents a generic container for a document, providing a media type for the correct handling of its value by the nodes.
+    /// This class can be used along with <see cref="DocumentCollection"/> to traffic different document types in a single message.
+    /// </summary>
+    /// <seealso cref="Lime.Protocol.Document" />
+    [DataContract(Namespace = "http://limeprotocol.org/2014")]
+    public class DocumentContainer : Document
+    {
+        public const string MIME_TYPE = "application/vnd.lime.container+json";        
+        public const string TYPE_KEY = "type";
+        public const string VALUE_KEY = "value";
+
+        public static readonly MediaType MediaType = MediaType.Parse(MIME_TYPE);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocumentContainer"/> class.
+        /// </summary>
+        public DocumentContainer() : 
+            base(MediaType)
+        {
+        }
+
+        /// <summary>
+        /// Gets the media type of the contained document.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        [DataMember(Name = TYPE_KEY)]
+        public MediaType Type => Value?.GetMediaType();
+
+        /// <summary>
+        /// Gets or sets the contained document value.
+        /// </summary>
+        /// <value>
+        /// The document.
+        /// </value>
+        [DataMember(Name = VALUE_KEY)]
+        public Document Value { get; set; }
+    }
+}
