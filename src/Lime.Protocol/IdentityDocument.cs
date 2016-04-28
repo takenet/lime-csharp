505611022a73a77d737dbdaab85edde4ef6295c0
André C. O. Minelli <andre@takenet.com.br>
2016-04-28 10:27:25 -0300

    Add parameterless constructor for IdentityDocument and static Parse method (needed for serialization)

--- src/Lime.Protocol/IdentityDocument.cs
+++ src/Lime.Protocol/IdentityDocument.cs
@@ -1,21 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.Serialization;
 
 namespace Lime.Protocol
 {
     /// <summary>
     /// Represents an identity document.
     /// </summary>
+    [DataContract(Namespace = "http://limeprotocol.org/2014")]
     public sealed class IdentityDocument : Document
     {
         public const string MIME_TYPE = "application/vnd.lime.identity";
         public static readonly MediaType MediaType = MediaType.Parse(MIME_TYPE);
 
-        #region Constructor
+        #region Constructors
+
+        public IdentityDocument()
+            : base(MediaType)
+        {
+
+        }
 
         public IdentityDocument(string identity)
-            : base(MediaType)
+            : this()
         {
             if (!string.IsNullOrWhiteSpace(identity))
             {
@@ -30,11 +36,17 @@ namespace Lime.Protocol
         /// <summary>
         /// The value of the document
         /// </summary>
-        public Identity Value { get; private set; }
+        public Identity Value { get; set; }
 
-        public override string ToString()
-        {
-            return this.Value.ToString();
-        }
+        public override string ToString() => Value.ToString();
+
+        /// <summary>
+        /// Parses the string to a 
+        /// IdentityDocument instance.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static IdentityDocument Parse(string value) => new IdentityDocument(value);
+
     }
 }
