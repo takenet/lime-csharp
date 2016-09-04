37d0e5f4afe00714daf9f4948c40ea19af22e731
André Bires <andrebires@gmail.com>
2016-09-04 11:49:04 -0300

    Added fake serializer

--- src/Lime.Protocol.LoadTests/Tcp/TcpTransportTests.cs
+++ src/Lime.Protocol.LoadTests/Tcp/TcpTransportTests.cs
@@ -30,7 +30,7 @@ namespace Lime.Protocol.LoadTests.Tcp
         {
             _uri = new Uri("net.tcp://localhost:55321");
             _cancellationToken = TimeSpan.FromSeconds(30).ToCancellationToken();
-            _envelopeSerializer = new JsonNetSerializer();
+            _envelopeSerializer = new FakeEnvelopeSerializer(10);
             _tcpTransportListener = new TcpTransportListener(_uri, null, _envelopeSerializer);
             await _tcpTransportListener.StartAsync();
 
@@ -44,9 +44,11 @@ namespace Lime.Protocol.LoadTests.Tcp
         }
 
         [TearDown]
-        public void Teardown()
+        public async Task TeardownAsync()
         {
-            
+            await _clientTcpTransport.CloseAsync(CancellationToken.None);
+            await _serverTcpTransport.CloseAsync(CancellationToken.None);
+            await _tcpTransportListener.StopAsync();
         }
 
         [Test]
@@ -76,5 +78,62 @@ namespace Lime.Protocol.LoadTests.Tcp
             sw.ElapsedMilliseconds.ShouldBeLessThan(count * 2);
         }
 
+    }
+
+    public sealed class FakeEnvelopeSerializer : IEnvelopeSerializer
+    {
+        private readonly Envelope[] _envelopes;
+        private readonly string[] _serializedEnvelopes;
+        private int _serializePos;
+        private int _deserializePos;
+
+        private readonly object _serializeSyncRoot = new object();
+        private readonly object _deserializeSyncRoot = new object();
+
+        public FakeEnvelopeSerializer(int count)
+        {
+            _envelopes = Enumerable
+                .Range(0, count)
+                .Select<int, Envelope>(i =>
+                {
+                    if (i % 5 == 0)
+                    {
+                        return Dummy.CreateNotification(Event.Accepted);
+                    }
+                    if (i % 3 == 0)
+                    {
+                        return Dummy.CreateCommand(Dummy.CreateAccount());
+                    }
+                    if (i % 2 == 0)
+                    {
+                        return Dummy.CreateMessage(Dummy.CreateTextContent());
+                    } 
+                    return Dummy.CreateMessage(Dummy.CreateSelect());
+                })
+                .ToArray();
+
+            var jsonSerializer = new JsonNetSerializer();
+            _serializedEnvelopes = _envelopes.Select(e => jsonSerializer.Serialize(e)).ToArray();
+        }
+
+        public string Serialize(Envelope envelope)
+        {
+            lock (_serializeSyncRoot)
+            {
+                var value = _serializedEnvelopes[_serializePos];
+                _serializePos = (_serializedEnvelopes.Length + 1) % _serializedEnvelopes.Length;
+                return value;
+            }
+        }
+
+        public Envelope Deserialize(string envelopeString)
+        {
+            lock (_deserializeSyncRoot)
+            {
+                var value = _envelopes[_deserializePos];
+                _deserializePos = (_envelopes.Length + 1) % _envelopes.Length;
+                return value;
+            }
+        }
     }
 }
