06a6c382fb6a86e9bd9c5b15328fa2a070ec8d7f
Rafael Avelar Pacheco <rafaelpa@takenet.com.br>
2016-12-05 12:10:04 -0200

    Port Lime.WebSocket.UnitTests project

--- src/Lime.Transport.WebSocket.UnitTests/WebSocketTransportListenerTests.cs
+++ src/Lime.Transport.WebSocket.UnitTests/WebSocketTransportListenerTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Lime.Protocol;
