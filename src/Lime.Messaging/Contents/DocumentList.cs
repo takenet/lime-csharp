7c89e2bcc94ec7e808c68379ff788ae0240e4bc4
André Bires Fonseca (Takenet) <andreb@takenet.com.br>
2017-01-23 15:49:29 -0200

    Added DocumentList and DocumentTuple classes

---
+++ src/Lime.Messaging/Contents/DocumentList.cs
@@ -0,0 +1,55 @@
+﻿using Lime.Protocol;
+using System.Runtime.Serialization;
+
+namespace Lime.Messaging.Contents
+{
+    /// <summary>
+    /// Defines a list of <see cref="Document"/> with a header and footer.
+    /// </summary>
+    /// <seealso cref="Document" />
+    [DataContract]
+    public class DocumentList : Document
+    {            
+        public const string MIME_TYPE = "application/vnd.lime.list+json";
+        public const string HEADER_KEY = "header";
+        public const string ITEMS_KEY = "items";
+        public const string FOOTER_KEY = "footer";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocumentList"/> class.
+        /// </summary>
+        public DocumentList()
+            : base(MediaType.Parse(MIME_TYPE))
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the list header document.
+        /// </summary>
+        /// <value>
+        /// The header.
+        /// </value>
+        [DataMember(Name = HEADER_KEY)]
+        public DocumentContainer Header { get; set; }
+ 
+        /// <summary>
+        /// Gets or sets the list items.
+        /// </summary>
+        /// <value>
+        /// The items.
+        /// </value>
+        [DataMember(Name = ITEMS_KEY)]
+        public DocumentContainer[] Items { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list footer document.
+        /// </summary>
+        /// <value>
+        /// The footer.
+        /// </value>
+        [DataMember(Name = FOOTER_KEY)]
+        public DocumentContainer Footer { get; set; }
+
+    }
+}
