d3878d851f52b5de3ba945a1f814044988b311cf
André Bires Fonseca (Takenet) <andreb@takenet.com.br>
2017-02-08 10:53:34 -0200

    Added support for external auth on test console

---
+++ src/Lime.Client.TestConsole/Macros/SendAuthenticatingExternalMacro.cs
@@ -0,0 +1,13 @@
+﻿using Lime.Protocol.Security;
+
+namespace Lime.Client.TestConsole.Macros
+{
+
+    [Macro(Name = "Send authenticating external", Category = "Session", IsActiveByDefault = false, Order = 1)]
+    public class SendAuthenticatingExternalMacro : SendAuthenticatingMacroBase
+    {
+        protected override string TemplateName => "Authenticating external";
+
+        protected override AuthenticationScheme Scheme => AuthenticationScheme.External;
+    }
+}
