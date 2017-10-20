1b3567b2a52f22428d0dcc263560a6c9d23d2884
André Bires Fonseca <andrebires@gmail.com>
2017-10-20 15:43:30 -0200

    Updated NotifyReceiptChannelModule to allow extending the notification creation

--- src/Lime.Protocol/Network/Modules/NotifyReceiptChannelModule.cs
+++ src/Lime.Protocol/Network/Modules/NotifyReceiptChannelModule.cs
@@ -1,45 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lime.Protocol.Network.Modules
 {
     /// <summary>
-    /// Defines a channel module that automatically send a <see cref="Event.Received"/> <see cref="Notification"/> for each received message.
+    /// Defines a channel module that automatically send a <see cref="Notification"/> for each received message.
     /// </summary>
-    /// <seealso cref="ChannelModuleBase{T}.Protocol.Message}" />
-    public sealed class NotifyReceiptChannelModule : ChannelModuleBase<Message>
+    public class NotifyReceiptChannelModule : ChannelModuleBase<Message>
     {
         private readonly IChannel _channel;
 
         public NotifyReceiptChannelModule(IChannel channel)
         {
-            if (channel == null) throw new ArgumentNullException(nameof(channel));
-            _channel = channel;
+            _channel = channel ?? throw new ArgumentNullException(nameof(channel));
         }
 
         public override async Task<Message> OnReceivingAsync(Message envelope, CancellationToken cancellationToken)
         {
-            var destination = envelope.To;
-
-            if (!envelope.Id.IsNullOrEmpty() &&
-                envelope.From != null &&
-                (destination == null || destination.Equals(_channel.LocalNode) || (destination.Instance.IsNullOrEmpty() && destination.ToIdentity().Equals(_channel.LocalNode.ToIdentity()))))
+            if (!envelope.Id.IsNullOrEmpty() 
+                && envelope.From != null 
+                && (envelope.To == null 
+                    || envelope.To.Equals(_channel.LocalNode) 
+                    || (envelope.To.Instance.IsNullOrEmpty() 
+                        && envelope.To.ToIdentity().Equals(_channel.LocalNode.ToIdentity()))))
             {
-                var notification = new Notification
-                {
-                    Id = envelope.Id,
-                    To = envelope.GetSender(),
-                    Event = Event.Received
-                };
-
+                var notification = CreateNotification(envelope);
                 await _channel.SendNotificationAsync(notification, cancellationToken);
             }
 
             return envelope;
+        }
+
+        protected virtual Notification CreateNotification(Message message)
+        {
+            return new Notification
+            {
+                Id = message.Id,
+                To = message.GetSender(),
+                Event = Event.Received
+            };
         }
     }
 }
