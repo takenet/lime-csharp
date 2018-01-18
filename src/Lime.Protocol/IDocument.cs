f26910d85cf50749d5facfab6ffe801372882d6f
André Bires Fonseca <andrebires@gmail.com>
2018-01-18 17:02:12 -0200

    Revert "Using IDocument instead of Document for assembly document registration"
    This reverts commit b2910bfeaf262556e41a498b4396f9f62633b5df.

--- src/Lime.Protocol/IDocument.cs
+++ src/Lime.Protocol/IDocument.cs
@@ -1,4 +1,10 @@
-﻿namespace Lime.Protocol
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lime.Protocol
 {
     /// <summary>
     /// Defines a entity with a <see cref="MediaType"/>.
