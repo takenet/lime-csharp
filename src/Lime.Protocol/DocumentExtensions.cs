e0eb2a8e0f95c7a16f52388b6482fc3856265cd3
Lucas Fonseca Mundim <mundim@Lucass-MacBook-Pro.local>
2019-05-09 22:55:46 -0300

    Add Document extension to make it easier to create DocumentContainers

---
+++ src/Lime.Protocol/DocumentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Lime.Protocol
+{
+    /// <summary>
+    /// Utilities extensions for the Document class
+    /// </summary>
+    public static class DocumentExtensions
+    {
+        /// <summary>
+        /// Creates a <c>DocumentContainer</c> with <paramref name="document"/> as its <c>Value</c>
+        /// </summary>
+        /// <returns>The document container.</returns>
+        /// <param name="document">Document to set as <c>Value</c></param>
+        public static DocumentContainer ToDocumentContainer(this Document document)
+        {
+            return new DocumentContainer { Value = document };
+        }
+    }
+}
