12ff18d7f292d3cbd15825c3f15956a45fdd8bf8
Lucas Mundim <lucasm@take.net>
2019-05-13 08:07:09 -0300

    Add optional collection size

--- src/Lime.Protocol/IEnumerableExtensions.cs
+++ src/Lime.Protocol/IEnumerableExtensions.cs
@@ -15,14 +15,15 @@ namespace Lime.Protocol
         /// </summary>
         /// <returns>The document collection.</returns>
         /// <param name="documentEnumerable"><c>IEnumerable</c> of <c>Document</c>.</param>
+        /// <param name="size">The number of <c>Documents</c> in the collection</param>
         /// <typeparam name="T">Subtype of the <c>Document</c>'s contained in the <c>IEnumerable</c></typeparam>
-        public static DocumentCollection ToDocumentCollection<T>(this IEnumerable<T> documentEnumerable) where T : Document
+        public static DocumentCollection ToDocumentCollection<T>(this IEnumerable<T> documentEnumerable, int size = 0) where T : Document
         {
             return new DocumentCollection
             {
                 Items = documentEnumerable.ToArray(),
                 ItemType = documentEnumerable.FirstOrDefault().GetMediaType(),
-                Total = documentEnumerable.Count()
+                Total = size == 0 ? documentEnumerable.Count() : size
             };
         }
     }
