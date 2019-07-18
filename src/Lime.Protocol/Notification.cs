b035a0e493f3597d0f3fda4fd64fbd4a54566a2c
André Bires <andrebires@gmail.com>
2019-07-18 13:10:26 -0300

    Removed guid constructor from notification

--- src/Lime.Protocol/Notification.cs
+++ src/Lime.Protocol/Notification.cs
@@ -23,12 +23,6 @@ namespace Lime.Protocol
 
         }
 
-        public Notification(Guid id)
-            : this(id.ToString())
-        {
-
-        }
-
         public Notification(string id)
             : base(id)
         {
