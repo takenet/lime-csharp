4cdb2c0e71ae6ae8a1e0fa5367d8d2d228d8fad3
André Bires <andrebires@gmail.com>
2019-12-04 22:05:17 -0300

    Playing with Json APIs

--- src/Lime.Protocol/Serialization/Json/EnvelopeSerializer.cs
+++ src/Lime.Protocol/Serialization/Json/EnvelopeSerializer.cs
@@ -1,4 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Lime.Protocol.Serialization.Json.Converters;
@@ -8,6 +13,9 @@ namespace Lime.Protocol.Serialization.Json
     public class EnvelopeSerializer : IEnvelopeSerializer
     {
         private readonly JsonSerializerOptions _options;
+
+
+        private readonly byte[] _statePropertyName = Encoding.UTF8.GetBytes(Session.STATE_KEY);
 
         public EnvelopeSerializer()
         {
@@ -40,9 +48,48 @@ namespace Lime.Protocol.Serialization.Json
 
         public Envelope Deserialize(string envelopeString)
         {
+            // TODO: get direct from stream
+            var envelopeUtf8 = new Memory<byte>(Encoding.UTF8.GetBytes(envelopeString));
+            //var reader = new Utf8JsonReader(envelopeUtf8.Span);
+            var jsonDocument = System.Text.Json.JsonDocument.Parse(envelopeUtf8);
+
+            if (jsonDocument.RootElement.TryGetProperty(_statePropertyName.AsSpan(), out _))
+            {
+                
+            }
+
+            //JsonSerializer.Deserialize(jsonDocument.RootElement, typeof(Session));
             
             
             throw new System.NotImplementedException();
         }
+    }
+    
+    // https://github.com/dotnet/corefx/issues/37564
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Local")]
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    internal readonly ref struct JsonElementSerializer
+    {
+        static readonly FieldInfo JsonDocumentField = typeof(JsonElement).GetField("_parent", BindingFlags.NonPublic | BindingFlags.Instance);
+        static readonly FieldInfo JsonDocumentUtf8JsonField = typeof(JsonDocument).GetField("_utf8Json", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        ReadOnlyMemory<byte> Value { get; }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public JsonElementSerializer(JsonElement jsonElement)
+        {
+            if(JsonDocumentField == null) throw new ArgumentNullException(nameof(JsonDocumentField));
+            if(JsonDocumentUtf8JsonField == null) throw new ArgumentNullException(nameof(JsonDocumentUtf8JsonField));
+            var jsonDocument = JsonDocumentField.GetValue(jsonElement);
+            Value = (ReadOnlyMemory<byte>) JsonDocumentUtf8JsonField.GetValue(jsonDocument);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T ToObject<T>(JsonSerializerOptions jsonSerializerOptions = null)
+        {
+            return JsonSerializer.Deserialize<T>(Value.Span, jsonSerializerOptions);
+        }
+
     }
 }
