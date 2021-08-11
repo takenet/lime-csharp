b3e79fa1a3a3ba11accc0bc54a341d8c0c210681
Andre Minelli <andre@take.net>
2021-08-11 11:23:24 -0300

    Add Envelope properties to EnvelopeTooLargeException.Data

--- src/Lime.Protocol/Network/EnvelopeTooLargeException.cs
+++ src/Lime.Protocol/Network/EnvelopeTooLargeException.cs
@@ -4,14 +4,39 @@ namespace Lime.Protocol.Network
 {
     public class EnvelopeTooLargeException : Exception
     {
-        public EnvelopeTooLargeException(string message) 
+        public EnvelopeTooLargeException()
+            : base()
+        {
+        }
+
+        public EnvelopeTooLargeException(string message)
             : base(message)
         {
         }
 
-        public EnvelopeTooLargeException(string message, Exception innerException) 
+        public EnvelopeTooLargeException(string message, Envelope envelope)
+            : base(message)
+        {
+            AddEnvelopePropertiesToData(envelope);
+        }
+
+        public EnvelopeTooLargeException(string message, Exception innerException)
             : base(message, innerException)
         {
+        }
+        public EnvelopeTooLargeException(string message, Envelope envelope, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        private void AddEnvelopePropertiesToData(Envelope envelope)
+        {
+            Data["EnvelopeType"] = envelope.GetType().Name;
+            Data[nameof(envelope.Id)] = envelope.Id?.ToString();
+            Data[nameof(envelope.To)] = envelope.To?.ToString();
+            Data[nameof(envelope.From)] = envelope.From?.ToString();
+            Data[nameof(envelope.Pp)] = envelope.Pp?.ToString();
+            Data[nameof(envelope.Metadata)] = envelope.Metadata;
         }
     }
 }
