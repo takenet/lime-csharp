f20add7e761ff6cca4fa9696052f1434acbf4255
André Bires <andrebires@gmail.com>
2021-09-04 12:03:56 -0300

    Handling exceptions on AspNetCore listeners

--- src/Lime.Transport.AspNetCore/Listeners/CommandListenerBase.cs
+++ src/Lime.Transport.AspNetCore/Listeners/CommandListenerBase.cs
@@ -10,19 +10,44 @@ namespace Lime.Transport.AspNetCore.Listeners
     /// </summary>
     public abstract class CommandListenerBase : ICommandListener
     {
-        protected CommandListenerBase()
-            : this (_ => true)
+        private readonly ISenderChannel? _senderChannel;
+
+        protected CommandListenerBase(ISenderChannel? senderChannel = null)
+            : this(_ => true, senderChannel)
         {
-            
         }
-        protected CommandListenerBase(Predicate<Command> filter)
+
+        protected CommandListenerBase(Predicate<Command> filter, ISenderChannel? senderChannel = null)
         {
+            _senderChannel = senderChannel;
             Filter = filter ?? throw new ArgumentNullException(nameof(filter));
         }
 
         public Predicate<Command> Filter { get; }
 
-        public Task OnEnvelopeAsync(Command envelope, CancellationToken cancellationToken) => OnCommandAsync(envelope, cancellationToken);
+        public async Task OnEnvelopeAsync(Command envelope, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await OnCommandAsync(envelope, cancellationToken);
+            }
+            catch (Exception ex) when (envelope.Id != null &&
+                                       envelope.Status == CommandStatus.Pending &&
+                                       _senderChannel is { State: SessionState.Established } &&
+                                       _senderChannel.Transport.IsConnected)
+            {
+                await _senderChannel.SendCommandAsync(
+                    new Command(envelope.Id)
+                    {
+                        Method = envelope.Method,
+                        Status = CommandStatus.Failure,
+                        Reason = ex.ToReason()
+                    },
+                    default);
+
+                throw;
+            }
+        }
 
         public abstract Task OnCommandAsync(Command command, CancellationToken cancellationToken);
     }
--- src/Lime.Transport.AspNetCore/Listeners/MessageListenerBase.cs
+++ src/Lime.Transport.AspNetCore/Listeners/MessageListenerBase.cs
@@ -10,20 +10,44 @@ namespace Lime.Transport.AspNetCore.Listeners
     /// </summary>
     public abstract class MessageListenerBase : IMessageListener
     {
-        protected MessageListenerBase()
-            : this (_ => true)
+        private readonly ISenderChannel? _senderChannel;
+
+        
+        protected MessageListenerBase(ISenderChannel? senderChannel = null)
+            : this (_ => true, senderChannel)
         {
             
         }
 
-        protected MessageListenerBase(Predicate<Message> filter)
+        protected MessageListenerBase(Predicate<Message> filter, ISenderChannel? senderChannel = null)
         {
             Filter = filter ?? throw new ArgumentNullException(nameof(filter));
+            _senderChannel = senderChannel;
         }
 
         public Predicate<Message> Filter { get; }
 
-        public Task OnEnvelopeAsync(Message envelope, CancellationToken cancellationToken) => OnMessageAsync(envelope, cancellationToken);
+        public async Task OnEnvelopeAsync(Message envelope, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await OnMessageAsync(envelope, cancellationToken);
+            }
+            catch (Exception ex) when (envelope.Id != null &&
+                                       _senderChannel is { State: SessionState.Established } &&
+                                       _senderChannel.Transport.IsConnected)
+            {
+                await _senderChannel.SendNotificationAsync(
+                    new Notification(envelope.Id)
+                    {
+                        Event = Event.Failed,
+                        Reason = ex.ToReason()
+                    },
+                    default);
+
+                throw;
+            }
+        }
 
         public abstract Task OnMessageAsync(Message message, CancellationToken cancellationToken);
     }
