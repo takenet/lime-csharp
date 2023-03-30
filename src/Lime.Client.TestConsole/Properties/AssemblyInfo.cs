0b6d71594d15cd6ae75e07913e9fa33deb6faf2f
André Césari de Oliveira Minelli (Take.net) <andre@take.net>
2023-03-30 09:33:17 -0300

    Change to follow latest published version on GitHub

--- src/Lime.Client.TestConsole/Properties/AssemblyInfo.cs
+++ src/Lime.Client.TestConsole/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Lime.Client.TestConsole")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.11.15.0")]
+[assembly: AssemblyFileVersion("1.11.15.0")]
