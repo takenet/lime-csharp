292dc78ff94508f9bdfa5fe691c61970fc34f35f
Acácio Oliveira <acacio.oliveira@take.net>
2023-05-17 15:57:06 -0300

    Added new reply message type feature

---
+++ src/Lime.Messaging/Contents/Reply.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.Serialization;
+using Lime.Protocol;
+
+namespace Lime.Messaging.Contents
+{
+    /// <summary>
+    /// Represents a reply message.
+    /// </summary>
+    [DataContract]
+    public class Reply : Document
+    {
+        public static readonly string MimeType = "application/vnd.lime.reply+json";
+        public static readonly MediaType MediaType = MediaType.Parse(MimeType);
+
+        public const string CONTENT = "content";
+        public const string REPLY_TO = "replyTo";
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Reply"/> class.
+        /// </summary>
+        public Reply()
+            : base(MediaType)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the content of the reply.
+        /// </summary>
+        [DataMember(Name = CONTENT)]
+        public DocumentContainer Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reference to the message being replied to.
+        /// </summary>
+        [DataMember(Name = REPLY_TO)]
+        public ReplyTo ReplyTo { get; set; }
+    }
+
+    /// <summary>
+    /// Represents the document container for the reply reference.
+    /// </summary>
+    [DataContract]
+    public class ReplyTo
+    {
+        public const string ID = "id";
+        public const string CONTENT = "content";
+
+        /// <summary>
+        /// Gets or sets the identifier of the message being replied to.
+        /// </summary>
+        [DataMember(Name = ID)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content of the reply.
+        /// </summary>
+        [DataMember(Name = CONTENT)]
+        public DocumentContainer Content { get; set; }
+    }
+}
--- src/Lime.Protocol.UnitTests/Serialization/Newtonsoft/EnvelopeSerializerTests.cs
+++ src/Lime.Protocol.UnitTests/Serialization/Newtonsoft/EnvelopeSerializerTests.cs
@@ -369,6 +369,57 @@ namespace Lime.Protocol.UnitTests.Serialization.Newtonsoft
 
         [Test]
         [Category("Serialize")]
+        public void Serialize_ReplyMessage_ReturnsValidJsonString()
+        {
+            var target = GetTarget();
+
+            var content = Dummy.CreateTextContent();
+            var documentConteiner = Dummy.CreateDocumentContainer(content);
+            var id = EnvelopeId.NewId();
+
+            var reply = new Reply
+            {
+                Content = documentConteiner,
+                ReplyTo = new ReplyTo
+                {
+                    Content = documentConteiner,
+                    Id = id
+                }
+            };
+
+            var message = Dummy.CreateMessage(reply);
+            message.Pp = Dummy.CreateNode();
+
+            var metadataKey1 = "randomString1";
+            var metadataValue1 = Dummy.CreateRandomString(50);
+            var metadataKey2 = "randomString2";
+            var metadataValue2 = Dummy.CreateRandomString(50);
+            message.Metadata = new Dictionary<string, string>();
+            message.Metadata.Add(metadataKey1, metadataValue1);
+            message.Metadata.Add(metadataKey2, metadataValue2);
+
+            var resultString = target.Serialize(message);
+            Assert.IsTrue(resultString.HasValidJsonStackedBrackets());
+            Assert.IsTrue(resultString.ContainsJsonProperty(Envelope.ID_KEY, message.Id));
+            Assert.IsTrue(resultString.ContainsJsonProperty(Envelope.FROM_KEY, message.From));
+            Assert.IsTrue(resultString.ContainsJsonProperty(Envelope.PP_KEY, message.Pp));
+            Assert.IsTrue(resultString.ContainsJsonProperty(Envelope.TO_KEY, message.To));
+            Assert.IsTrue(resultString.ContainsJsonProperty(Message.TYPE_KEY, message.Content.GetMediaType()));
+            Assert.IsTrue(resultString.ContainsJsonKey(Message.CONTENT_KEY));
+            Assert.IsTrue(resultString.ContainsJsonProperty(metadataKey1, metadataValue1));
+            Assert.IsTrue(resultString.ContainsJsonProperty(metadataKey2, metadataValue2));
+
+            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(resultString, target.Settings);
+            var replyObject = dictionary[Reply.CONTENT].ShouldBeAssignableTo<JObject>();
+            var contentObject = replyObject[Reply.CONTENT].ShouldBeAssignableTo<JObject>();
+            var replyToObject = replyObject[Reply.REPLY_TO].ShouldBeAssignableTo<JObject>();
+
+            replyToObject[ReplyTo.ID].ShouldBe(id);
+            contentObject[DocumentContainer.VALUE_KEY].ShouldBe(content.Text);
+        }
+
+        [Test]
+        [Category("Serialize")]
         public void Serialize_TextMessage_ReturnsValidJsonString()
         {
             var target = GetTarget();
@@ -1448,6 +1499,55 @@ namespace Lime.Protocol.UnitTests.Serialization.Newtonsoft
 
             var textContent = (PlainText)message.Content;
             Assert.AreEqual(text, textContent.Text);
+        }
+
+        [Test]
+        [Category("Deserialize")]
+        public void Deserialize_ReplyMessage_ReturnsValidInstance()
+        {
+            var target = GetTarget();
+
+            var id = EnvelopeId.NewId();
+            var from = Dummy.CreateNode();
+            var pp = Dummy.CreateNode();
+            var to = Dummy.CreateNode();
+
+            string randomKey1 = "randomString1";
+            string randomKey2 = "randomString2";
+            string randomString1 = Dummy.CreateRandomStringExtended(50);
+            string randomString2 = Dummy.CreateRandomStringExtended(50);
+
+            var text = Dummy.CreateRandomStringExtended(50);
+            var text1 = text.Escape();
+            var text2 = text.Escape();
+
+            string json =
+                $"{{\"id\":\"{id}\",\"to\":\"{to}\",\"from\":\"{@from}\",\"pp\":\"{pp}\",\"type\":\"application/vnd.lime.reply+json\",\"metadata\":{{\"{randomKey1}\":\"{randomString1.Escape()}\",\"{randomKey2}\":\"{randomString2.Escape()}\"}},\"content\":{{\"content\":{{\"type\":\"text/plain\",\"value\":\"{text1}\"}},\"replyTo\":{{\"id\":\"{id}\",\"content\":{{\"type\":\"text/plain\",\"value\":\"{text2}\"}}}}}}}}";
+
+            var envelope = target.Deserialize(json);
+
+            var message = envelope.ShouldBeOfType<Message>();
+            Assert.AreEqual(id, message.Id);
+            Assert.AreEqual(from, message.From);
+            Assert.AreEqual(pp, message.Pp);
+            Assert.AreEqual(to, message.To);
+            Assert.IsNotNull(message.Metadata);
+            Assert.IsTrue(message.Metadata.ContainsKey(randomKey1));
+            Assert.AreEqual(message.Metadata[randomKey1], randomString1);
+            Assert.IsTrue(message.Metadata.ContainsKey(randomKey2));
+            Assert.AreEqual(message.Metadata[randomKey2], randomString2);
+
+            message.Content.ShouldBeOfType<Reply>();
+
+            var reply = (Reply)message.Content;
+
+            Assert.AreEqual(id, reply.ReplyTo.Id);
+
+            var replyText = reply.Content.Value.ShouldBeOfType<PlainText>();
+            Assert.AreEqual(text1, replyText.Text);
+
+            var replyToText = reply.ReplyTo.Content.Value.ShouldBeOfType<PlainText>();
+            Assert.AreEqual(text2, replyToText.Text);
         }
 
         [Test]
